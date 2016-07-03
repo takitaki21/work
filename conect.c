ffc516dcff9396890ce14f6a74ed2793a82288ed
takitaki21 <hrxdr280@yahoo.co.jp>
2016-07-03 15:52:05 +0900

    conect.c

--- conect.c
+++ conect.c
@@ -1,101 +1,110 @@
-#include <stdio.h>
-#include <stdlib.h>
+	#include <stdio.h>
+	#include <stdlib.h>
 
-struct list{
-	int key;
-	struct list *next;
-};
- 
-struct list *get_list(void);
-void print_list(struct list *p);
-void insert_after(struct list *x,struct list *p);
-void insert_before(struct list *x,struct list *p);
-void delete_next(struct list *p);
-void delete_it(struct list *p);
-int get_data(void);
+	struct list{
+		int key;
+		struct list *next;
+	};
+	 
+	struct list *get_list(void);
+	void print_list(struct list *p);
+	void insert_after(struct list *x,struct list *p);
+	void insert_before(struct list *x,struct list *p);
+	void delete_next(struct list *p);
+	void delete_it(struct list *p);
+	int get_data(void);
 
-#define EOD -1
-int a[]={1 ,2 , 3, 4, 5, 6, EOD};
+	#define EOD -1
+	int a[]={1 ,2 , 3, 4, 5, 6, EOD};
 
 
-struct list *get_list(void){
-	int d;
-	struct list *p,*newp;
+	struct list *get_list(void){
+		int d;
+		struct list *p,*newp;
 
-	p=NULL;
-	while((d=get_data())!=EOD){
-		newp=(struct list*)malloc(sizeof(struct list));
-	newp->key =d;
-	newp->next=p;
-	p=newp;
-	}
-	return p;
-}
-
-int get_data(void){
-static int i=0;
-return a[i++];
-}
-
-void print_list(struct list *p){
-	while(p!=NULL){
-		printf("%d ",p->key);
-		p=p->next;
+		p=NULL;
+		while((d=get_data())!=EOD){
+			newp=(struct list*)malloc(sizeof(struct list));
+		newp->key =d;
+		newp->next=p;
+		p=newp;
+		}
+		return p;
 	}
 
-	printf("\n");
-}
-void insert_after(struct list *x,struct list *p){
-	x->next=p->next;
-	p->next=x;
-}
+	int get_data(void){
+	static int i=0;
+	return a[i++];
+	}
 
-void insert_before(struct list *x,struct list *p){
-	struct list tmp;
-	tmp=*x;
-	*x=*p;
-	*p=tmp;
-	p->next=x;
-}
+	void print_list(struct list *p){
+		while(p!=NULL){
+			printf("%d\t",p->key);
+			p=p->next;
+		}
 
-void delete_next(struct list *p){
-	struct list *q;
+		printf("\n");
+	}
+	void insert_after(struct list *x,struct list *p){
+		x->next=p->next;
+		p->next=x;
+	}
 
-	q=p->next;
-	p->next=q->next;
-	free(q);
-}
+	void insert_before(struct list *x,struct list *p){
+		struct list tmp;
+		tmp=*x;
+		*x=*p;
+		*p=tmp;
+		p->next=x;
+	}
 
-void delete_it(struct list *p){
-	struct list *q;
-	if(p->next!=NULL){
+	void delete_next(struct list *p){
+		struct list *q;
+
 		q=p->next;
-		*p=*q;
+		p->next=q->next;
 		free(q);
 	}
-}
 
-int main (void){
-	struct list *lp,*new1,*new2;
-   
-    lp = get_list();
-    print_list(lp);
+	void delete_it(struct list *p){
+		struct list *q;
+		if(p->next!=NULL){
+			q=p->next;
+			*p=*q;
+			free(q);
+		}
+	}
 
-    new1=(struct list *)malloc(sizeof(struct list));
-    new1 -> key=111;
-    insert_after(new1,lp->next->next);
-    print_list(lp);
+	int main (void){
+	char c=10;
+	int plase,num;
+		struct list *lp,*new1;
+		lp=get_list();
+		print_list(lp);
 
-    new2=(struct list *)malloc(sizeof(struct list));
-    new2->key=222;
-    insert_before(new2,lp->next->next);
-    print_list(lp);
+		printf("what is done?\tE.end\tA.addition\tD.delete\n");
+		while(c!='E'){
+		scanf("%c",&c);
 
-    delete_next(lp->next->next);
-    print_list(lp);
+	    fflush(stdin);
+		switch(c){
+			case'A':
+			printf("add number & which is it addition");
+			scanf("%d%d",&num,&plase);
+			new1=(struct list *)maloc(sizeof(struct list));
+			new1->key=num;
+			insert_after(new1,lp->next);
+			print_list(lp);
 
-    delete_it(lp->next->next);
-    print_list(lp);
+			case'D':
+			printf("which list is deleate ");
+			scanf("%d",&plase);
+			delete_it(lp->next);
+			print_list(lp);
+		}
 
-    return 0;
+	        fflush(stdin);
+	        printf("what is done?\tE.end\tA.addition\tD.delete\n");
+	}
+	    return 0;
 }
